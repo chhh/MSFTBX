cf7bea6a07c86dc0c3155fd344afd9b9c97ea62d
unknown <dmitriya@msa-304884.adsroot.itcs.umich.edu>
2015-09-02 18:07:19 -0400

    Improve SpectrumUtils

--- MSFileToolbox/src/umich/ms/util/SpectrumUtils.java
+++ MSFileToolbox/src/umich/ms/util/SpectrumUtils.java
@@ -17,20 +17,32 @@ public class SpectrumUtils {
     /**
      * Given an m/z value, calculates what a certain ppm accuracy converts to in terms of m/z.
      * @param mz base m/z at which the calculation is done
-     * @param ppm
+     * @param ppmDelta ppm difference, which is to be converted to amu.
      * @return
      */
-    public static double ppm2amu(double mz, double ppm) {
-        return (mz / 1e6) * ppm;
+    public static double ppm2amu(double mz, double ppmDelta) {
+        return (mz / 1e6) * ppmDelta;
     }
 
     /**
      * Given an absolute m/z difference ({@code amu} parameter), find out how much ppm that is.
      * @param mz base m/z at which the calculation is done.
-     * @param amu absolute m/z difference, which is to be converted to ppm.
+     * @param amuDelta absolute m/z difference, which is to be converted to ppm.
      * @return
      */
-    public static double amu2ppm(double mz, double amu) {
-        return amu / (mz / 1e6);
+    public static double amu2ppm(double mz, double amuDelta) {
+        return amuDelta / (mz / 1e6);
+    }
+
+    /**
+     * Check if the 2nd m/z value is within some PPM distance from the 1st one. PPM will be calculated based on the 1st
+     * m/z value.
+     * @param mz1 PPM tolerance will be calculated relative to this value
+     * @param mz2 the value to check for being within some PPM range
+     * @param ppm
+     * @return
+     */
+    public static boolean isWithinPpm(double mz1, double mz2, double ppm) {
+        return Math.abs(amu2ppm(mz1, mz1 - mz2)) <= ppm;
     }
 }
