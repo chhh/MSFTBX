8f770b2b2f1b65309a4d070302977774e3ff84fc
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2016-04-15 21:27:20 -0400

    Added Apache 2,0 license to the root dir and updated license headers on all source files, except the umich.ms.external package

--- MSFileToolbox/src/umich/ms/fileio/chunk/package-info.java
+++ MSFileToolbox/src/umich/ms/fileio/chunk/package-info.java
@@ -1,5 +1,16 @@
-/**
- * This is under construction, chunked files are not yet used anywhere in the code.
- * Created by Dmitry Avtonomov on 2016-04-10.
+/* 
+ * Copyright 2016 Dmitry Avtonomov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package umich.ms.fileio.chunk;
