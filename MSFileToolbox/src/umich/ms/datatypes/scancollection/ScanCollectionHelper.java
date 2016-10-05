df3db8746ea6625aa9ed5eeb3a862ed7d510b06e
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2016-10-05 12:17:00 -0400

    More consistent filtering of auto-detected DIA-runs

--- MSFileToolbox/src/umich/ms/datatypes/scancollection/ScanCollectionHelper.java
+++ MSFileToolbox/src/umich/ms/datatypes/scancollection/ScanCollectionHelper.java
@@ -15,14 +15,12 @@
  */
 package umich.ms.datatypes.scancollection;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
+
 import umich.ms.datatypes.scan.IScan;
 import umich.ms.datatypes.scan.props.PrecursorInfo;
 import umich.ms.fileio.exceptions.FileParsingException;
+import umich.ms.util.Interval1D;
 import umich.ms.util.IntervalST;
 
 /**
@@ -73,6 +71,8 @@ public class ScanCollectionHelper {
 
         Set<Integer> msLevels = scans.getMapMsLevel2index().keySet();
         Integer[] msLevelsArr = msLevels.toArray(new Integer[msLevels.size()]);
+        Arrays.sort(msLevelsArr);
+
         for (int i=0; i < msLevelsArr.length - 1; i++) {
             // if we're at the bottom MS level, these Scans can't have children, stop processing,
             // hence i < msLevelsArr.length - 1 in the for loop
@@ -140,72 +140,73 @@ public class ScanCollectionHelper {
 
     /**
      * Calling this method only makes sense if the whole LC/MS structure has been parsed.
-     * It only works for grouping MS2 scans, no MS3 is supported.
      * @param scans the scan collection, it will be modified in-place
      */
     public static void finalizePrecursorWindows(IScanCollection scans) {
         TreeMap<Integer, IntervalST<Double, TreeMap<Integer, IScan>>> mapMsLevel2rangeGroups = scans.getMapMsLevel2rangeGroups();
-        Integer msLevel = 2;
-        IntervalST<Double, TreeMap<Integer, IScan>> rangeMapMS2 = mapMsLevel2rangeGroups.get(msLevel);
-        if (rangeMapMS2 != null) {
-            if (rangeMapMS2.size() == 0) {
-                // we don't need an empty map
-                mapMsLevel2rangeGroups.remove(msLevel);
+
+        List<Integer> msLevelsToRemove = new ArrayList<>();
+
+        msLevelLoop:
+        for (Map.Entry<Integer, IntervalST<Double, TreeMap<Integer, IScan>>> entry : mapMsLevel2rangeGroups.entrySet()) {
+
+            int msLevel = entry.getKey();
+            IntervalST<Double, TreeMap<Integer, IScan>> rangeMapMS2 = entry.getValue();
+
+            if (rangeMapMS2.size() == 1) {
+                msLevelsToRemove.add(msLevel);
             } else {
                 // if we have a precursor range map, we want to see if it actually contains
                 // ranges that have lots of MS2 spectra in them.
                 // ALL ranges are required to have more than 1 MS2 spectra
-                boolean allRangesContainOverOneScan = true;
-                for (IntervalST.Node<Double, TreeMap<Integer, IScan>> range : rangeMapMS2) {
-                    if (range.getValue().size() < 2) {
-                        allRangesContainOverOneScan = false;
-                        break;
+
+                for (IntervalST.Node<Double, TreeMap<Integer, IScan>> node : rangeMapMS2) {
+                    if (node.getValue().size() <= 1) {
+                        // if we didn't have at least 1 scans in even one range, then discard the whole sub-tree
+                        msLevelsToRemove.add(msLevel);
+                        continue msLevelLoop;
                     }
                 }
-                if (!allRangesContainOverOneScan) {
-                    // if we didn't have at least 2 scans in even one range, then discard the whole MS2 tree
-                    mapMsLevel2rangeGroups.remove(msLevel);
-                    return;
-                }
-
 
                 // we also check that all ranges contain approximately equal amount of scans
                 // THIS WILL NOT WORK FOR MSX DATA, UNLESS "maxScanCountDiff" is set to some large value, which
                 // should be at least [ms1_mz_range / (msx_isolation_width * msx_multiplex_number)]
-                boolean allRangesHaveApproxSameScanCounts = true;
-                int maxScansInGroup = 0, minScansInGroup = Integer.MAX_VALUE;
-                for (IntervalST.Node<Double, TreeMap<Integer, IScan>> node : rangeMapMS2) {
-                    TreeMap<Integer, IScan> range = node.getValue();
-                    if (range.size() > maxScansInGroup) {
-                        maxScansInGroup = range.size();
-                    }
-                    if (range.size() < minScansInGroup) {
-                        minScansInGroup = range.size();
-                    }
-                }
-
-                if (minScansInGroup == 0 || maxScansInGroup == 0 || minScansInGroup == Integer.MAX_VALUE) {
-                    throw new IllegalStateException("Something went wrong when guessing precursor groupings for" +
-                            " DIA experiment.");
-                }
-
-                if (rangeMapMS2.size() == 1) {
-                    // case .size() == 0 was checked above
-                    // MS2 range map had only one precursor range and more than 1 scan mapped to it - should be fine
-                } else {
-                    double ratio = ((double)Math.abs(maxScansInGroup - minScansInGroup)) / minScansInGroup;
-                    if (ratio > MAX_PRECURSOR_GROUP_SIZE_DIFF) {
-                        allRangesHaveApproxSameScanCounts = false;
-                    }
-                }
-
-                if (!allRangesHaveApproxSameScanCounts) {
-                    mapMsLevel2rangeGroups.remove(msLevel);
-                    return;
+                if (!isAllRangesHaveApproxSameScanCounts(rangeMapMS2)) {
+                    msLevelsToRemove.add(msLevel);
+                    continue msLevelLoop;
                 }
 
                 // otherwise we just keep it there, but the tree needs to be recreated, because the ranges are incorrect
             }
         }
+        for (Integer msLevel : msLevelsToRemove)
+            mapMsLevel2rangeGroups.remove(msLevel);
+        int a = 1;
+    }
+
+    private static boolean isAllRangesHaveApproxSameScanCounts(IntervalST<Double, TreeMap<Integer, IScan>> rangeMapMS2) {
+        boolean allRangesHaveApproxSameScanCounts = true;
+        int maxScansInGroup = 0, minScansInGroup = Integer.MAX_VALUE;
+        for (IntervalST.Node<Double, TreeMap<Integer, IScan>> node : rangeMapMS2) {
+            TreeMap<Integer, IScan> range = node.getValue();
+            if (range.size() > maxScansInGroup)
+                maxScansInGroup = range.size();
+            if (range.size() < minScansInGroup)
+                minScansInGroup = range.size();
+        }
+
+        if (minScansInGroup == 0 || maxScansInGroup == 0 || minScansInGroup == Integer.MAX_VALUE)
+            throw new IllegalStateException("Something went wrong when guessing precursor groupings for DIA experiment.");
+
+        if (rangeMapMS2.size() == 1) {
+            // case .size() == 0 was checked above
+            // MS2 range map had only one precursor range and more than 1 scan mapped to it - should be fine
+        } else {
+            double ratio = ((double)Math.abs(maxScansInGroup - minScansInGroup)) / minScansInGroup;
+            if (ratio > MAX_PRECURSOR_GROUP_SIZE_DIFF) {
+                allRangesHaveApproxSameScanCounts = false;
+            }
+        }
+        return allRangesHaveApproxSameScanCounts;
     }
 }
