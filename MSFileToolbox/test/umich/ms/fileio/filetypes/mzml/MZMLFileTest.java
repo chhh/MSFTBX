88d6594a942d06da5eb40a1ab31d43409e3cd555
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2017-06-21 19:33:37 -0400

    Add test files

---
+++ MSFileToolbox/test/umich/ms/fileio/filetypes/mzml/MZMLFileTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Dmitry Avtonomov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package umich.ms.fileio.filetypes.mzml;
+
+import umich.ms.datatypes.lcmsrun.LCMSRunInfo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Dmitry Avtonomov
+ */
+public class MZMLFileTest {
+    List<Path> paths;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            final URI uri = classLoader.getResource("mzml").toURI();
+            final Path path = Paths.get(uri);
+
+            final DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+            paths = new ArrayList<>();
+            for (Path p : stream) {
+                if (Files.isRegularFile(p)) { // && p.getFileName().toString().equals("RawCentriodCidWithMsLevelInRefParamGroup.mzML")
+                    paths.add(p);
+                }
+            }
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalStateException("Could not set up test env in MZMLFileTest");
+        }
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        paths.clear();
+        paths = null;
+    }
+
+    @org.junit.Test
+    public void parseRunInfo() throws Exception {
+        for (Path p : paths) {
+            System.out.printf("\n\nFile: %s\n", p.getFileName());
+
+            final MZMLFile mzml = new MZMLFile(p.toString());
+            final LCMSRunInfo runInfo = mzml.fetchRunInfo();
+
+            System.out.printf("\tRun info: %s\n", runInfo);
+        }
+    }
+
+}
