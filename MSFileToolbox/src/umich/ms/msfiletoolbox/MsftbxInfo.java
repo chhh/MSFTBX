109907b65a4193dcd4a87dd31435ffbc3ab664d0
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2017-06-23 01:30:48 -0400

    Release notes for v1.2.3

--- MSFileToolbox/src/umich/ms/msfiletoolbox/MsftbxInfo.java
+++ MSFileToolbox/src/umich/ms/msfiletoolbox/MsftbxInfo.java
@@ -22,21 +22,31 @@ import java.util.*;
  * @author Dmitry Avtonomov
  */
 public class MsftbxInfo {
-    public static final String version = "1.2.1";
+    public static final String version = "1.2.3";
 
     private static TreeMap<String, List<String>> changelog = new TreeMap<>();
     private static Map<String, List<String>> changelogImmutable = Collections.unmodifiableMap(changelog);
 
     static {
-        String v120 = "v1.2.0";
-        List<String> v120notes = new LinkedList<>();
-        v120notes.add("Added fragmentation energy and reaction time");
-        changelog.put(v120, v120notes);
+        String v123 = "v1.2.3";
+        List<String> v123notes = new LinkedList<>();
+        v123notes.add("Add test resources path to Maven POM.");
+        v123notes.add("If anything goes wrong during index parsing, tries to re-index the file instead of throwing exceptions.");
+        v123notes.add("Support for ReferenceableParamGroups in mzML.");
+        v123notes.add("Fix BOM for mzML/mzXML files.");
+        v123notes.add("Change deps to Javolution-MSFTBX v6.11.6");
+        v123notes.add("Use CharArray.contentEquals() in mzML parser");
+        changelog.put(v123, v123notes);
 
         String v121 = "v1.2.1";
         List<String> v121notes = new LinkedList<>();
         v121notes.add("Mavenized, published to Maven Central");
         changelog.put(v121, v121notes);
+
+        String v120 = "v1.2.0";
+        List<String> v120notes = new LinkedList<>();
+        v120notes.add("Added fragmentation energy and reaction time");
+        changelog.put(v120, v120notes);
     }
 
     public static String getVersion() {
