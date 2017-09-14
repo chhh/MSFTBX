822c81b951b53b9daa85f05ed9e54ae8ed976441
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2017-09-14 14:00:53 -0400

    First implementation of iterable PepXml file parser ready.

---
+++ MSFileToolbox/src/umich/ms/util/XmlUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Dmitry Avtonomov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package umich.ms.util;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+/**
+ * @author Dmitry Avtonomov
+ */
+public class XmlUtils {
+    private XmlUtils() {}
+
+    /**
+     * Advances the Stream Reader to the closest first occurrence of a tag.
+     * @param xsr The reader to advance.
+     * @param tag The tag to advance to.
+     * @return True if advanced successfully, false when the end of document was successfully reached.
+     * @throws XMLStreamException In all cases other than described by 'return'.
+     */
+    public static boolean advanceReaderToNextRunSummary(XMLStreamReader xsr, String tag) throws XMLStreamException {
+        if (tag == null)
+            throw new IllegalArgumentException("Tag name can't be null");
+        if (xsr == null)
+            throw new IllegalArgumentException("Stream Reader can't be null");
+        do {
+            if (xsr.next() == XMLStreamConstants.END_DOCUMENT)
+                return false;
+        } while (!(xsr.isStartElement() && xsr.getLocalName().equals(tag)));
+
+        return true;
+    }
+}
